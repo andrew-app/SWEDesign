39f7823508232223ff3ec5e6e286f8e4099024bd
andrew-app <andrew.appuhamy@gmail.com>
2021-08-10 23:35:45 +1000

    ousb code

--- lab1/partb.cpp
+++ lab1/partb.cpp
@@ -1,48 +1,108 @@
 #include <iostream>
 #include <fstream>
-#include <cstring>
+#include <string>
+#include <windows.h>
 
+#define strSize 256
 using namespace std;
 
+int OUSBcommand(char* command);//function prototype
 
 
-int main () {
-  string line;
-  ifstream myfile ("test.txt");
-  int temp = 0;
-  if (myfile.is_open())
-  {
-    while ( getline (myfile,line) )
+
+int OUSBcommand(char* command)
+{
+    FILE* fpipe;
+    char line[strSize] = {};
+
+    fpipe = (FILE*)_popen(command, "r");
+    if (fpipe != NULL)
     {
-      bool isnum = true;
-      char myArray[line.size()+1];//as 1 char space for null is also required
-      strcpy(myArray, line.c_str());
-      for (int i = 0; myArray[i] != '\0'; i++)
-      {
-        if(isdigit(myArray[i]))
-          temp = 1;
-        else
-        {
-          cout << "Invalid Number" << endl;
-          isnum = false;
-          break;
+        while (fgets(line, sizeof(line), fpipe))
+        {   // do nothing here, or print out debug data
+            //cout << line; // print out OUSB data for debug purposes
         }
-      }
-      if (isnum == 1)
-      {
-        int val = stoi(line);
+        _pclose(fpipe);   // close pipe
 
-        if (val > 15)
-          cout << "Out of range" << endl;
-
-        else
-          cout << val << endl;
-      }
+        // caller can see whole ousb string in str so any errors can be detected
+        strcpy_s(command, strSize, line);
     }
-    myfile.close();
-  }
+    else strcpy_s(command, strSize, "Error, problems with pipe!\n");
 
-  else cout << "Unable to open file";
+    char checktxt[50] = "Device not found";
 
-  return 0;
+    string debug = line; //copy char array of ousb.exe output to a string
+
+    int value = 0;
+
+    value = (int)atoi(line);
+
+    if (debug.find(checktxt) != string::npos) {
+        cout << "OUSB is not connected." << endl;
+
+        value = -1; // if ousb not connected set portb to -1 same as npos(not positive)
+
+    }
+
+
+
+    // convert value from char array to int
+
+    return value;
+}
+
+
+int main() {
+    string argline;
+    ifstream myfile("test.txt");
+    int temp = 0;
+    int val = 0;
+    int retvalue = 0;
+     //write output from reading to LED
+    char myArray[strSize];//as 1 char space for null is also required
+
+
+    
+    if (myfile.is_open())
+    {
+        while (getline(myfile, argline))
+        {
+            bool isnum = true;
+            memset(myArray, 0, 256);
+            strcpy_s(myArray, strSize, argline.c_str());
+            for (int i = 0; myArray[i] != '\0'; i++)
+            {
+                if (isdigit(myArray[i]))
+                    temp = 1;
+                else
+                {
+                    cout << "Y" << endl;
+                    isnum = false;
+                    break;
+                }
+            }
+            if (isnum == 1)
+            {
+                
+                val = stoi(argline);
+
+                if (val > 15)
+                    cout << "Y" << endl;
+
+                else
+                {
+                    char writeportb[strSize] = "ousb -r io PORTB ";
+                    strcat_s(writeportb, myArray);
+                    retvalue = OUSBcommand(writeportb);
+                    cout << val << endl;
+                    Sleep(100);
+                }
+            }
+        }
+        myfile.close();
+    }
+
+    else cout << "F" << endl;
+
+    return 0;
 }
